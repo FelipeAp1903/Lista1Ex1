55b9c00e42c9f7e0f26ffd01331ea27602f5b748
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 16:37:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double b;
+            Double h;
+            Double a;
+            
+            Console.WriteLine("coloque o valor da base(b):");
+            b = Double.Parse(Console.ReadLine());
+            Console.WriteLine("coloque o valor da altura(h):");
+            h = Double.Parse(Console.ReadLine());
+            a = b * h;
+            Console.WriteLine("o valor da Área é: {0}", a);
+
+
+
+        }
+    }
+}
